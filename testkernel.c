a510dbd865c0d66606529abc7ac1e826abad97a0
bornagainunitologist <cintari939@gmail.com>
2018-03-23 14:42:55 -0400

    Update testkernel.c

--- testkernel.c
+++ testkernel.c
@@ -6,38 +6,81 @@
 #include<unistd.h>
 
 
-int main(){
+
+
+// populates a buffer with x characters and tries to read y characters
+int fillthenread(int x, int y){
+
 	int fd = open("/dev/chardevdriver", O_RDWR); 
 	int i = 0;
-	int stringlen = 20;
-
+	int stringlen = x;
+	int ret = 0;
 	char stringtosend[stringlen+1];
+	char buffer[1030];
+	int charstoreq = y;
+	char *labelPtr = stringtosend;
+	
+	
 
-	for(i = 0; i < stringlen; i++){
-		stringtosend[i] = i+'a';
-	}
-	stringtosend[15] = '\0';
-
+	// attempt to open the device
 	if (fd < 0){
 		perror("Failed to open the device...");
 		return errno;
 	}else{
 		// writing
-		char *labelPtr = stringtosend;
+		
+		if(x == 0){
+			printf("x = 0, do not send any characters to driver\n");
+		}else{
+			// populate the string to send
+			for(i = 0; i < stringlen; i++){
+				stringtosend[i] = 'a';
+			}
+			stringtosend[i] = '\0';
 
-		printf("The sent message is: [%s]\n", stringtosend);
-		int ret = write(fd, labelPtr, strlen(labelPtr));
-		printf("RETURN VAL FROM DRIVER: %d\n", ret);
-
+			// send it
+			printf("The sent message is: [%s]\n", stringtosend);
+			int ret = write(fd, labelPtr, stringlen);
+			printf("RETURN VAL FROM DRIVER: %d\n", ret);
+		}
 
 		// reading
-		char buffer[1025];
+	
+		if(y != 0){
 
-		ret = read(fd, buffer, 15);
-		printf("The received message is: [%s]\n", buffer);
-		printf("RETURN VAL FROM DRIVER: %d\n", ret);
+			ret = read(fd, buffer, charstoreq);
+			printf("requesting %d characters from the driver\n", charstoreq);
+			printf("The received message is: [%s]\n", buffer);
+			printf("received %d characters from the driver\n", strlen(buffer));
+			printf("RETURN VAL FROM DRIVER: %d\n", ret);
+		}else{
+			printf("y = 0, do not request characters from driver\n");
+		}
+
+		// close the device
+		ret = close(fd);
 	}
+}
 
+int main(){
+	// empty the driver
+	printf("emptying the driver\n");
+	fillthenread(0, 1050);
+	printf("\n");
 
+	// fill the driver 
+	//printf("filling the driver past limit\n");
+	//readthenfill(1030, 0);
+	//printf("\n");
+
+	// partiall fill the driver
+	printf("filling the driver to limit\n");
+	fillthenread(1024, 0);
+	printf("\n");
+
+	// empty the driver
+	printf("emptying the driver\n");
+	fillthenread(0, 1050);
+	printf("\n");
 	return 0;
 }
