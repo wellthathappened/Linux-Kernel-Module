8b86d39644ce60295851e8a9c9467a441c568460
bornagainunitologist <cintari939@gmail.com>
2018-04-03 01:08:04 -0400

    Update read.c

--- read.c
+++ read.c
@@ -2,8 +2,6 @@
 // Students: Serra Abak, Ian Lewis, Jonathan Killeen
 // Instructor: Matthew B. Gerber Ph.D.
 
-// Assignment: Write a character-mode Linux device driver as a kernel module.
-
 // Code structure and organization based on references from:
 // Peter Jay Salzman's and Michael Burian, Ori Pomerantz's "The Linux Kernel Module Programming Guide"
 // http://www.tldp.org/LDP/lkmpg/2.6/html/x569.html
@@ -17,15 +15,18 @@
 #include <linux/init.h>               
 #include <linux/device.h>           
 #include <linux/vmalloc.h>
+#include <linux/mutex.h>	
+#include <linux/device.h>
+#include <linux/kdev_t.h>
 
-#define DEVICE_NAME "ReadDevice"
+#define DEVICE_NAME "rochardevdriver"
 #define BUFFER_SIZE 1024
 #define EBUSY 16
 
 MODULE_LICENSE("GPL");            
 MODULE_AUTHOR("SA, IL, JK");  
-MODULE_DESCRIPTION("Linux char driver to read PA3"); 
-MODULE_VERSION("2"); 
+MODULE_DESCRIPTION("Linux read only char driver PA 3"); 
+MODULE_VERSION("1"); 
 
 // Represents a circular buffer
 typedef struct cbuffer {
@@ -44,8 +45,6 @@ cbuffer_t* createCirBuffer(int n);
 // Destroys a cbuffer.
 void destroyCirBuffer(cbuffer_t *cb);
 
-// Writes passed character to end of cbuffer and returns the passed character if space is available. Returns -1 if an error occurs.
-int writeToBuffer(cbuffer_t *cb, char c);
 
 // Reads the last character from the cbuffer, removes it from the buffer and returns it. Returns -1 if an error occurs.
 char readFromBuffer(cbuffer_t *cb);
@@ -60,14 +59,28 @@ void printActualBuffer(cbuffer_t *cb);
 char peekBuffer(cbuffer_t *cb);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Function prototypes
 void cleanup_module(void);
 int init_module(void);
 int openDevice(struct inode *inode, struct file *file);
 int closeDevice(struct inode *inode, struct file *file);
-ssize_t readFromDevice(struct file *, char *, size_t, loff_t *);
-
-
+int readFromDevice(struct file *, char *, size_t, loff_t *);
 
 // File operations defined for use in our program
 struct file_operations fops = {
@@ -82,32 +95,71 @@ int driverNumber;                               // Device Driver Number
 char buffer[BUFFER_SIZE];                       // Character Buffer
 bool deviceOpen = false;                        // Checks if a device is in use
 static cbuffer_t *cb = NULL;                                  	// Circle buffer for input
+static DEFINE_MUTEX(cb_mutex);
 
+
+//static struct class c_dev;
+//static struct dev_t dev;
+
+
+
+
+// make the CB extnerally accessible
+EXPORT_SYMBOL(cb);
+
+EXPORT_SYMBOL(cb_mutex);
 
 // First method to be called when the module is loaded
 int init_module(void)
 {
+//	cl = class_create(THIS_MODULE ,"rochardevdriver"); 
+//	device_create(cl, NULL, dev, NULL, "rochardevdriver");
+	
     driverNumber = register_chrdev(0, DEVICE_NAME, &fops);
     
 	// initialize the CB
 	cb = createCirBuffer(BUFFER_SIZE);
+	
 
+	
     // Error-check
     if(driverNumber < 0)
     {
-        printk(KERN_INFO "Device failed to register: %d\n", driverNumber);
+        printk(KERN_INFO "readchar: Device failed to register: %d\n", driverNumber);
         
         return driverNumber;
     }
+	
     // Success
     else
     {
-        printk(KERN_INFO "Module successfully loaded!\n");
-	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, driverNumber);
+        printk(KERN_INFO "readchar: Module successfully loaded!\n");
+		printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, driverNumber);
+		
+		
+		
+		printk(KERN_INFO "readchar: Initializing mutex!\n");	 
+		mutex_init(&cb_mutex);       /// Initialize the mutex lock dynamically at runtime
+		printk(KERN_INFO "readchar: Mutex initialized!\n");
+		
+		
+		printk(KERN_INFO "readchar: locking mutex\n");
+		if(!mutex_trylock(&cb_mutex)){
+			printk(KERN_INFO "readchar: failed to lock mutex\n");
+			
+		}else{
+			printk(KERN_INFO "readchar: mutex locked\n");
+			
+			printk(KERN_INFO "readchar: unlocking mutex\n");
+
+			mutex_unlock(&cb_mutex);          /// Releases the mutex (i.e., the lock goes up)
+			
+			printk(KERN_INFO "readchar: unlocked mutex\n");
+		}
+		
+		
         return 0;
     }
-
-
 
 }
 
@@ -115,9 +167,13 @@ int init_module(void)
 void cleanup_module(void)
 {
     unregister_chrdev(driverNumber, DEVICE_NAME);
+	
     // destroy the circular buffer
 	destroyCirBuffer(cb);
-    printk(KERN_ALERT "Module successfully unloaded.\n");
+	
+	mutex_destroy(&cb_mutex);        /// destroy the dynamically-allocated mutex
+	
+    printk(KERN_ALERT "readchar: Module successfully unloaded.\n");
 }
 
 // Method called when opening a device
@@ -125,7 +181,7 @@ int openDevice(struct inode *inode, struct file *file)
 {
     if(deviceOpen)
     {
-        printk(KERN_ALERT "A device is currently opened in this module.\n");
+        printk(KERN_ALERT "readchar: A device is currently opened in this module.\n");
         
         return EBUSY;
     }
@@ -134,7 +190,7 @@ int openDevice(struct inode *inode, struct file *file)
     {
         try_module_get(THIS_MODULE);
         
-        printk(KERN_INFO "The device was successfully opened!\n");
+        printk(KERN_INFO "readchar: The device was successfully opened!\n");
         
         deviceOpen = true;
         
@@ -147,35 +203,49 @@ int closeDevice(struct inode *inode, struct file *file)
 {
     module_put(THIS_MODULE);
     
-    printk(KERN_ALERT "The device was successfully released!\n");
+    printk(KERN_ALERT "readchar: The device was successfully released!\n");
     
     deviceOpen = false;
-    
+   
+	
     return 0;
 }
 
 // Method called when reading from the device to the buffer
-ssize_t readFromDevice(struct file *filp, char *buffer, size_t length, loff_t *offset)
+int readFromDevice(struct file *filp, char *buffer, size_t length, loff_t *offset)
 {
 	
 	int i = 0;
 	char temp = -2;
 
-	printk(KERN_INFO "charMode: Received request for %zu characters from the user\n", length);
-	printk(KERN_INFO "charMode: Current CB count before read is: %d\n", cb->charsinbuffer);
+	
+	
+	printk(KERN_INFO "readchar: Received request for %zu characters from the user\n", length);
+	printk(KERN_INFO "readchar: Current CB count before read is: %d\n", cb->charsinbuffer);
+	
+	
+	printk(KERN_INFO "readchar: locking mutex\n");
+	
+	// Try to acquire the mutex (i.e., put the lock on/down)
+	if(!mutex_trylock(&cb_mutex)){    
+		printk(KERN_INFO "readchar: failed to lock mutex\n");
+		return -1;
+	}
+	
+	printk(KERN_INFO "readchar: mutex locked\n");
 
 	if(cb->charsinbuffer > 0){
 
 		if(length == 1){
 			// only 1 char requested
 			buffer[0] = readFromBuffer(cb);
-			printk(KERN_INFO "charMode: adding char to buffer: %d\n", temp);
+			printk(KERN_INFO "readchar: adding char to buffer: %d\n", temp);
 			i++;
 		}else{
 			// 2 or more char requested
 			temp =  readFromBuffer(cb);
 			while(i <= length && temp != -1){		
-				printk(KERN_INFO "charMode: adding char to buffer: %d\n", temp);
+				printk(KERN_INFO "readchar: adding char to buffer: %d\n", temp);
 			
 				buffer[i] = temp;
 				temp =  readFromBuffer(cb);
@@ -187,13 +257,21 @@ ssize_t readFromDevice(struct file *filp, char *buffer, size_t length, loff_t *o
 	}
 	
 	
-	printk(KERN_INFO "charMode: adding null char to buffer\n");
+	printk(KERN_INFO "readchar: adding null char to buffer\n");
 	buffer[i] = '\0';
 
-	printk(KERN_INFO "charMode: Current CB count after read is: %d\n", cb->charsinbuffer);
+	printk(KERN_INFO "readchar: Current CB count after read is: %d\n", cb->charsinbuffer);
 
+	
+	printk(KERN_INFO "readchar: unlocking mutex\n");
+	
+	mutex_unlock(&cb_mutex);          /// Releases the mutex (i.e., the lock goes up)
+	
+	printk(KERN_INFO "readchar: mutex unlocked\n");
+	
     return 0;
 }
+
 
 //========================================================================================================================================================================================================
 //========================================================================================================================================================================================================
